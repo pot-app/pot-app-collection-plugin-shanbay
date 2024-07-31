775a2d49a5ddd9793ca6e37e9388681d33b22023
Pylogmon <pylogmon@outlook.com>
2024-07-31 08:06:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
 async function collection(source, target, options = {}) {
-  const { config, utils } = options;
-  const { tauriFetch: fetch } = utils;
-  const { auth_token: authToken } = config;
+    const { config, utils } = options;
+    const { tauriFetch: fetch } = utils;
+    const { auth_token: authToken } = config;
 
-  if (authToken === undefined || authToken.length === 0) {
-    throw "authToken not found";
-  }
+    if (authToken === undefined || authToken.length === 0) {
+        throw "authToken not found";
+    }
 
-  let body = {
-    business_id: 6,
-    words: [source],
-  };
+    let body = {
+        business_id: 6,
+        words: [source],
+    };
 
-  let res = await fetch(
-    "https://apiv3.shanbay.com/wordscollection/words_bulk_upload",
-    {
-      method: "POST",
-      headers: {
-        Cookie: `auth_token=${authToken}`,
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      body: {
-        type: "Json",
-        payload: body,
-      },
-    },
-  );
-
-  if (res.ok) {
-    const result = res.data;
-    if (result.msg) {
-      throw result.msg;
-    } else if (result["task_id"]) {
-      let check_res = await fetch(
+    let res = await fetch(
         "https://apiv3.shanbay.com/wordscollection/words_bulk_upload",
         {
-          method: "GET",
-          query: { business_id: "6", task_id: result["task_id"] },
-          headers: {
-            Cookie: `auth_token=${authToken}`,
-          },
+            method: "POST",
+            headers: {
+                Cookie: `auth_token=${authToken}`,
+                "Content-Type": "application/json;charset=UTF-8",
+            },
+            body: {
+                type: "Json",
+                payload: body,
+            },
         },
-      );
-      const check_result = check_res.data;
-      const { failed_count } = check_result;
-      if (failed_count > 0) {
-        throw "Failed to add words";
-      } else {
-        return true;
-      }
+    );
+
+    if (res.ok) {
+        const result = res.data;
+        if (result.msg) {
+            throw result.msg;
+        } else if (result["task_id"]) {
+            let check_res = await fetch(
+                "https://apiv3.shanbay.com/wordscollection/words_bulk_upload",
+                {
+                    method: "GET",
+                    query: { business_id: "6", task_id: result["task_id"] },
+                    headers: {
+                        Cookie: `auth_token=${authToken}`,
+                    },
+                },
+            );
+            const check_result = check_res.data;
+            const { failed_count } = check_result;
+            if (failed_count > 0) {
+                throw "Failed to add words";
+            } else {
+                return true;
+            }
+        } else {
+            throw JSON.stringify(result);
+        }
     } else {
-      throw JSON.stringify(result);
+        throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
     }
-  } else {
-    throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
-  }
 }
